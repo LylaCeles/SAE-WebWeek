d03e609e66462230a127b53059b1086689fd834b
margot-florent <116064280+margot-florent@users.noreply.github.com>
2022-10-24 15:36:58 +0200

    Add files via upload

---
+++ Accueil/index.php
@@ -0,0 +1,72 @@
+<php
+
+?>
+
+<html>
+    <head>
+        <link rel = "stylesheet" type = "text/css" href = "style/index.css">
+    </head>
+    <body>
+        <section class="container">
+            <!-- <header class="header">
+                <span class="logo"><img src="" alt=""></span>
+                <span class="menu">
+                    <ul>
+                        <li>Régions</li>
+                        <li>Animations</li>
+                        <li>Préinscription</li>
+                        <li>FAQ</li>
+                        <li>Contact</li>
+                    </ul>
+                </span>
+            </header> -->
+            <div class="imgIntro">
+                <img src="" alt="">
+                <p>Description</p>
+            </div>
+            <section class="planning">
+                <!-- Définir notre hiérarchie de titres -->
+                <h1>Planning</h1>
+                <img src="" alt="">
+                <a href=""><button>Plus d'informations sur les animations</button></a>
+            </section>
+            <section class="plat">
+                <h1>Découvrez de nouveaux plats</h1>
+                <p>Trouvez ici trois plats aléatoires gastronomiques français.
+                    <br>Pour en voir plus cliquez sur un des plats !
+                </p>
+                <!-- Faire les animations des images aléatoires en js -->
+                <img src="" alt="">
+                <img src="" alt=""> 
+                <img src="" alt="">
+            </section>
+            <section class="question">
+                <h1>Des questions ?</h1>
+                <p>Trouvez ici les questions qui nous sont plus posées</p>
+                <!-- <select id="">
+                    <option>Question 1</option>
+                </select>
+                <select id="">
+                    <option>Question 2</option>
+                </select>
+                <select id="">
+                    <option>Question 3</option>
+                </select> -->
+                <a href=""><button>Plus de questions ici</button></a>
+            </section>
+            <section class="sponsors">
+                <h1>Sponsors</h1>
+                <p>
+                    <img src="images/logo-bonduelle.png" alt="logo bonduelle" width=150px>
+                    <img src="images/logo-lustucru.png" alt="logo lustucru" width=150px> 
+                    <img src="images/logo-nestle.png" alt="logo nestlé" width=150px>
+                    <img src="images/logo-volvic.png" alt="logo volvic" width=150px>
+                    <img src="images/logo-styorre.png" alt="logo St Yorre" width=150px>
+                </p>
+            </section>
+            <footer>
+                <!-- Inclure le footer -->
+            </footer>
+        </section>
+    </body>
+</html>
