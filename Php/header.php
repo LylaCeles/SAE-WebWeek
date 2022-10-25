4f46c9fe62ccd12ba0f00d1df9e5953c2c0a2b81
TheoLsp <116071932+TheoLsp@users.noreply.github.com>
2022-10-25 17:46:06 +0200

    Création header.php

---
+++ Php/header.php
@@ -0,0 +1,21 @@
+<header>
+    <img src="./Image/logo/Logo_fond_blanc.png" alt="">
+    <nav>
+        <ul class="menu">
+            <li><a href="#">Régions</a></li>
+            <li><a href="#">Animations</a></li>
+            <li><a href="#">Préinscription</a></li>
+            <li><a href="#">FAQ</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+    <section class="language">
+        <div class="selected_lang">
+            Français
+        </div>
+        <ul>
+            <li><a href="#">English</a></li>
+        </ul>
+    </section>
+    
+</header>
