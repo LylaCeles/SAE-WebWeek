daa2c7863bd4ae31f9708f71ffca02ff7672868b
VincentDelUCA <vincent.danet@etu.uca.fr>
2022-10-26 09:26:26 +0200

    MAJ

---
+++ region.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8">
+
+    <title>European Meal Tour</title>
+
+    <link rel="stylesheet" href="./CSS/region.css">   
+    <link rel="stylesheet" href="./CSS/accordion.css">   
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+    <script src="./Script/accordion.js" defer></script>
+
+  </head>
+  <body>
+    <header>
+
+      <div id="TextePrincipal">
+        <h1>Gastronomie des régions</h1>
+        <div id="premierTrait"></div>
+        <h3>Découvrez ici la gastronomie Française ! <br>
+            Choisissez un plat : vous aurez son histoire , ses ingrédients et sa recette </h3>
+      </div>
+
+      <div id="imagePrincipale">
+      </div>
+
+
+    </header>
+    <section id="presentation">
+      <div id="texteIntro">
+        <h2>Les produits par régions</h2>
+      </div>
+    </section>
+
+    <section id="region">
+      <div class="accordion">
+        <div class="accordion-item">
+            <div class="accordion-item-header">
+                <h2>Intitulé de l'accordéon</h2>
+            </div>
+            <div class="accordion-item-body">
+                <div class="accordion-item-body-content">
+                  <div class="lescartes">
+
+                    <div class="container">
+                      <div class="card">
+                        <div class="front"></div>
+                        <div class="back">
+                          <h1>Back of Card</h1>
+                          <p>Additional info on the back of the card</p>
+                        </div>
+                      </div>
+                    </div>
+            
+            
+                    <div class="container">
+                      <div class="card">
+                        <div class="front"></div>
+                        <div class="back">
+                          <h1>Back of Card</h1>
+                          <p>Additional info on the back of the card</p>
+                        </div>
+                      </div>
+                    </div>
+            
+                    <div class="container">
+                      <div class="card">
+                        <div class="front"></div>
+                        <div class="back">
+                          <h1>Back of Card</h1>
+                          <p>Additional info on the back of the card</p>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div class="container">
+                      <div class="card">
+                        <div class="front"></div>
+                        <div class="back">
+                          <h1>Back of Card</h1>
+                          <p>Additional info on the back of the card</p>
+                        </div>
+                      </div>
+                    </div>
+
+                </div>
+                </div>
+            </div>
+        </div>
+</div>
+
+
+
+
+
+
+  </section>
+
+
+
+
+
+
+    <footer>
+      <div id="copyrightEtIcons">
+        <div id="copyright">
+          <span>The European Meal Tour</span>
+        </div>
+        <div id="icons">
+          <a href="http://www.twitter.fr"><i class="fa fa-twitter"></i></a>
+          <a href="http://www.facebook.fr"><i class="fa fa-facebook"></i></a>
+          <a href="http://www.instagram.com"><i class="fa fa-instagram"></i></a>
+
+        </div>
+      </div>
+
+    </footer>
+
+  </body>
+
+</html>
