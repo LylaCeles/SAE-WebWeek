91578bdba8fbfd29623dc41806380dfc4e049be0
margot-florent <116064280+margot-florent@users.noreply.github.com>
2022-10-27 14:39:05 +0200

    admin et connexion

--- connexion.php
+++ connexion.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 
 	<head>
-	  	<link rel="stylesheet" type="text/css" href="CSS/Global.css">
+	  	<link rel="stylesheet" type="text/css" href="CSS/global.css">
 	  	<link rel="stylesheet" type="text/css" href="CSS/connexion.css">
 	  	<meta http-equiv="content-type" content="text/html"; charset="UTF-8"/>
 		<link rel="icon" type="image/x-icon" href="favi_16x16.png">
@@ -14,20 +14,22 @@
 
 	</head>
 	<body>
+		<h2>Vous pouvez vous connecter au compte administrateur.</h2>
 
 <!-- ************* Formulaire pour se connecter au compte admin *************** -->
         <fieldset>
-            <legend>Veuillez-vous connecter</legend>
+            <legend><h3>Connexion</h3></legend>
 			<form action ="admin.php" method="POST">
 				<p>
 					<label for ="id">Votre identifiant :</label>
-					<input type="text" name ="id" placeholder="identifiant" required="required">
+					<input type="text" name ="id" placeholder="Identifiant" required="required">
 				</p>
 				<p>
 					<label for ="mdp">Votre mot de passe :</label>
 					<input type="password" name ="mdp" placeholder="Confidenciel !" required="required">
 				</p>
-				<input type="submit" name="envoie" id="envoie_co">
+				<br>
+				<input type="submit" name="envoie" class="btn">
 
 
 			</form>
