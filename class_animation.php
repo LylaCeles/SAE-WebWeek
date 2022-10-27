558fc3872664640e6fef7668b5b70ebdeab0ca17
LylaCeles <lyla.mikadzuki@gmail.com>
2022-10-27 11:37:34 +0200

    Revert "Revert "Revert "mise en place du POO"""
    This reverts commit 5432b30e40dbc048237d27c738813e64effe51cf.

--- class_animation.php
+++ class_animation.php
@@ -24,76 +24,31 @@ class Animation {
         $this->nb_place = $nb_place;
         $this->horaire_debut = $horaire_debut;
         $this->horaire_fin = $horaire_fin;
-        // echo($this->nom);
-        // echo($this->id);
-        $i=0;
-        foreach ($inscrit as $tab) {
-            $this->inscrit[$i]["id"]= $tab["id_personne"];
-            $this->inscrit[$i]["nb_personne"]= $tab["nb_personne"];
-            $i++;
-        }{
+        // print_r ($inscrit);
+        for ($i=0; $i<count($inscrit); $i++){
             // On rempli la liste des inscrits avec l'id des différentes personnes
-            
-
+            $this->inscrit[]= $inscrit[$i]->id;
         }
         
 
     }
 
     
-    public function affichePerso($personne){
-        //permet d'afficher toute les info sur les personnes inscrites
+    public function affiche(){
+        //permet d'afficher tout le planning des animations du festival
         echo("<h1>".$this->nom."</h1>");
-     
-    
+
         foreach($this->inscrit as $tab){
-            
-            for ($i=0; $i <count($personne) ; $i++) { 
-                if ($tab["id"]== $personne[$i]->id){
-                    
-                    $personne[$i]->affiche();
-                }
-            }
+        print_r($tab);
         }
     }
 
-    public function afficheInfo($img){
+    public function rechercherParType($type){
+        //Recherche par type d'animation
+    }
 
-        echo('<div class="card">
-              <div class="left">
-                  <h1>Théâtre culinaire</h1>
-                  <p>'.$this->description.'</p>
-                  <button>Se préinscrire</button>
-              </div>
-              <div class="right">
-                  <img src="'.$img.'" alt="">
-              </div>
-          </div>
-
-
-          <table class="table-style">
-
-            <thead>
-                <tr>
-                    <th>Horaire</th>
-                    <th>Lieux</th>
-                    <th>Tarif</th>
-                </tr>
-            </thead>
-            
-    
-            <tbody>
-                <tr>
-                    <td>'.$this->horaire_debut.'</td>
-                    <td>Rue des Potiers</td>
-                    <td>Pas cher wlh</td>
-                </tr>
-            </tbody>
-    
-        </table>
-
-
-
+    public function rechercherParDate($date){
+        
     }
 
 
