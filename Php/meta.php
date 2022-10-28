52fd2750760d337daa9c24461f2da68b94ae7485
LylaCeles <lyla.mikadzuki@gmail.com>
2022-10-28 11:53:11 +0200

    correction logo et formulaire contact

--- Php/meta.php
+++ Php/meta.php
@@ -7,6 +7,6 @@
 <link rel="stylesheet" href="./CSS/header.css">
 <link rel="stylesheet" href="./CSS/global.css">
 <link rel="stylesheet" href="./CSS/footer.css">
-<link rel="icon" type="image/x-icon" href="favicon.png"/>
+<link rel="icon" type="image/x-icon" href="Image/logo/favicon.png"/>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
 
--- contact.php
+++ contact.php
@@ -24,9 +24,9 @@
       <section id="formulaire">
           <h2 id="contact">Contactez-nous</h2>
           <form action="" method="POST">
-            <input placeholder="Nom">
-            <input placeholder="E-mail">
-            <textarea placeholder="Votre message ici..."></textarea>
+            <input type="text" placeholder="Nom" required="required">
+            <input type="email" placeholder="E-mail"required="required">
+            <textarea placeholder="Votre message ici..."required="required"></textarea>
             <button class="btn">Envoyer</button>
           </form>
           </div>
